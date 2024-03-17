65eb622d09a1d8b4561d1292709b6ec810cc668f
Vikas K <37661974+vikasgowda3007@users.noreply.github.com>
2024-03-17 14:40:31 +0530

    Image slideshow

--- dist/team-kavach/server/main.server.mjs
+++ dist/team-kavach/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NIUVIZKL.mjs";import"./chunk-FBHT3LWV.mjs";import"./chunk-NYV43UK2.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-727LLP2X.mjs";import"./chunk-34MGCGQJ.mjs";import"./chunk-KKTNYE6T.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,7 @@ import { DonateDialogComponent } from '../donate-dialog/donate-dialog.component'
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import { NgImageSliderModule } from 'ng-image-slider';
 
 
 @Component({
@@ -25,40 +26,42 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
+    NgImageSliderModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   constructor(public dialog: MatDialog) {}
 
-  private dListPics: string[] = ['assets/jpg/d1.jpg', 'assets/jpg/d2.jpg', 'assets/jpg/d3.jpg', 'assets/jpg/d4.jpg'];
-  public dPic: string = '';
-  private index: number = 0;
-
-  ngOnInit(): void {
-    this.dPic = this.dListPics[this.index];
-}
+  public imgCollection: Array<object> = [
+    {
+      image: 'assets/jpg/d1.jpg',
+      thumbImage: 'assets/jpg/d1.jpg',
+      alt: 'Image 1',
+      title: 'Image 1'
+    },
+    {
+      image: 'assets/jpg/d2.jpg',
+      thumbImage: 'assets/jpg/d2.jpg',
+      alt: 'Image 2',
+      title: 'Image 2'
+    },
+    {
+      image: 'assets/jpg/d3.jpg',
+      thumbImage: 'assets/jpg/d3.jpg',
+      alt: 'Image 3',
+      title: 'Image 3'
+    },
+    {
+      image: 'assets/jpg/d4.jpg',
+      thumbImage: 'assets/jpg/d4.jpg',
+      alt: 'Image 4',
+      title: 'Image 5'
+    },
+];
 
   public openDonateDialog(): void {
     this.dialog.open(DonateDialogComponent);
-  }
-
-  public leftPic(): void {
-    if (this.index === 0) {
-        this.index = 3;
-    } else {
-        this.index--;
-    }
-    this.dPic = this.dListPics[this.index];
-  }
-
-  public rightPic(): void {
-    if (this.index === 3) {
-        this.index = 0;
-    } else {
-        this.index++;
-    }
-    this.dPic = this.dListPics[this.index];
   }
 }
